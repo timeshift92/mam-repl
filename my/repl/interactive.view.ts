08579ad7f8ba44228a160e837e142aea858faa6a
Nurbek <akhmedov@infosystems.uz>
2020-03-14 23:28:24 +0500

    changes

--- my/repl/interactive.view.ts
+++ my/repl/interactive.view.ts
@@ -2,9 +2,10 @@ namespace $.$$ {
 
 	export class $my_repl_interactive extends $.$my_repl_interactive {
 		socket: any
-		host = 'http://127.0.0.1:9080'
+		host = 'http://46.173.215.130'
+		
 		reconnect() {
-			this.socket = new WebSocket('ws://localhost:9001')
+			this.socket = new WebSocket('ws://46.173.215.130/ws')
 			var self = this;
 			this.socket.onclose = function () {
 				setTimeout(self.reconnect, 5000)
@@ -16,28 +17,29 @@ namespace $.$$ {
 				}, 250);
 
 			}
+
+			this.sending_source.tree = this.$.$mol_state_arg.dict['tree_source'];
+			this.sending_source.ts = this.$.$mol_state_arg.dict['ts_source'];
+			this.sending_source.css = this.$.$mol_state_arg.dict['css_source'];
+
+			setInterval(() => {
+				this.outbox(this.sending_source)
+				// if (
+				// 	(this.current_source.tree != this.sending_source.tree ||
+				// 		this.current_source.css != this.sending_source.css ||
+				// 		this.current_source.ts != this.sending_source.ts)
+				// 	&& new Date().getTime() - this.typing > 1500) {
+				// 	this.current_source = Object.assign({}, this.sending_source);
+				// 	if (this.socket.readyState === this.socket.OPEN)
+				// 		this.socket.send(JSON.stringify(this.sending_source));
+				// }
+			}, 1000)
 		}
 
 		constructor() {
 			super();
-			this.sending_source.tree = this.$.$mol_state_arg.value('tree_source')
-			this.sending_source.ts = this.$.$mol_state_arg.value('ts_source')
-			this.sending_source.css = this.$.$mol_state_arg.value('css_source')
+
 			this.reconnect();
-			setInterval(() => {
-				if (
-					(this.current_source.tree != this.sending_source.tree ||
-					this.current_source.css != this.sending_source.css ||
-					this.current_source.ts != this.sending_source.ts)
-					&& new Date().getTime() - this.typing > 1500) {
-					this.current_source = Object.assign({}, this.sending_source);
-					if (this.socket.readyState === this.socket.OPEN)
-						this.socket.send(JSON.stringify(this.sending_source));
-
-				}
-			}, 1000)
-
-
 
 		}
 		@$mol_mem
@@ -49,32 +51,29 @@ namespace $.$$ {
 			return ''
 
 		}
+
+		@$mol_mem
+		outbox(next?: { tree: string, css: string, ts: string }, ) {
+			this.socket.send(JSON.stringify(next))
+			return next
+		}
 		typing = new Date().getTime();
 		sending_source = { tree: '', ts: '', css: '' };
 		current_source = { tree: '', ts: '', css: '' };;
-		source(next?: string) {
-			$my_repl.Root(0).App().TreeSource().Edit().dom_node().setAttribute('autofocus','true');
-			let source = this.$.$mol_state_arg.value('source', next)
-			this.typing = new Date().getTime();
-			this.sending_source.tree = source;
-			return source || ''
-		}
+
 		tree_source(next?: string) {
-			$my_repl.Root(0).App().TreeSource().Edit().dom_node().setAttribute('autofocus','true');
 			let source = this.$.$mol_state_arg.value('tree_source', next)
 			this.typing = new Date().getTime();
 			this.sending_source.tree = source;
 			return source || ''
 		}
 		css_source(next?: string) {
-			$my_repl.Root(0).App().CssSource().Edit().dom_node().setAttribute('autofocus','true');
 			let source = this.$.$mol_state_arg.value('css_source', next)
 			this.typing = new Date().getTime();
 			this.sending_source.css = source;
 			return source || ''
 		}
 		ts_source(next?: string) {
-			$my_repl.Root(0).App().TsSource().Edit().dom_node().setAttribute('autofocus','true');
 			let source = this.$.$mol_state_arg.value('ts_source', next)
 			this.typing = new Date().getTime();
 			this.sending_source.ts = source;
---
+++ my/repl/page/page.view.ts
@@ -0,0 +1,10 @@
+namespace $.$$ {
+	export class $my_repl_page extends $.$my_repl_page {
+		
+		message() {
+			let name = this.name()
+			return name && `Hello, ${name}!`
+		}
+		
+	}
+}
